21f1cadca79549f65ecf8f5dcb9e2f25dac5256f
LouisLe999 <khoe.le.mall@gmail.com>
2023-03-28 17:13:16 +0700

    #Fix vercel env build

--- next.config.js
+++ next.config.js
@@ -1,13 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  experimental: {
-    appDir: true
-  },
-  pageExtensions:['js','jsx','ts','tsx'],
-  images: {
-    domains: ["links.papareact.com","louisle.s3.ap-southeast-1.amazonaws.com","cdn.hashnode.com"],
-  }
-}
+    reactStrictMode: true,
+    experimental: {
+        appDir: true,
+    },
+    pageExtensions: ["js", "jsx", "ts", "tsx"],
+    images: {
+        domains: [
+            "links.papareact.com",
+            "louisle.s3.ap-southeast-1.amazonaws.com",
+            "cdn.hashnode.com",
+        ],
+    },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,19 +3,7 @@ const { join } = require("path");
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     mode: "jit",
-    content: [
-        // "./app/**/*.{js,ts,jsx,tsx}", // Note the addition of the `app` directory.
-        // "./pages/**/*.{js,ts,jsx,tsx}",
-        // "./components/**/*.{js,ts,jsx,tsx}",
-        join(__dirname, "/app/**/*.{js,ts,jsx,tsx}"),
-        join(__dirname, "/components/**/*.{js,ts,jsx,tsx}"),
-        join(__dirname, "/pages/**/*.{js,ts,jsx,tsx}"),
-    ],
-    purge: [
-        join(__dirname, "/app/**/*.{js,ts,jsx,tsx}"),
-        join(__dirname, "/components/**/*.{js,ts,jsx,tsx}"),
-        join(__dirname, "/pages/**/*.{js,ts,jsx,tsx}"),
-    ],
+    purge: ['./app/**/*.{js,ts,jsx,tsx}', './Components/**/*.{js,ts,jsx,tsx}'],
     theme: {
         extend: {},
     },
