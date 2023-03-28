a3bcbcc04ed536439cc9c3c5f22017a79c9e5426
LouisLe999 <khoe.le.mall@gmail.com>
2023-03-28 15:41:43 +0700

    #Fix add jit mode tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,23 @@
+const { join } = require("path");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./app/**/*.{js,ts,jsx,tsx}", // Note the addition of the `app` directory.
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-
-    // Or if using `src` directory:
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
+    mode: "jit",
+    content: [
+        // "./app/**/*.{js,ts,jsx,tsx}", // Note the addition of the `app` directory.
+        // "./pages/**/*.{js,ts,jsx,tsx}",
+        // "./components/**/*.{js,ts,jsx,tsx}",
+        join(__dirname, "/app/**/*.{js,ts,jsx,tsx}"),
+        join(__dirname, "/components/**/*.{js,ts,jsx,tsx}"),
+        join(__dirname, "/pages/**/*.{js,ts,jsx,tsx}"),
+    ],
+    purge: [
+        join(__dirname, "/app/**/*.{js,ts,jsx,tsx}"),
+        join(__dirname, "/components/**/*.{js,ts,jsx,tsx}"),
+        join(__dirname, "/pages/**/*.{js,ts,jsx,tsx}"),
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+};
